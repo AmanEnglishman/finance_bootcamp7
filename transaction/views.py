355cf1644ef2e3c86ad5a38ed619fad4d9be17af
electrovenik <englishmanaman@gmail.com>
2025-12-16 19:43:32 +0600

    commit

--- transaction/views.py
+++ transaction/views.py
@@ -1,1 +1,29 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+from .permissions import IsOwner
+from .filters import TransactionFilter
+from .models import Transaction, Category
+from .serializers import CategorySerializer, TransactionSerializer
+
+
+class CategoryViewSet(ModelViewSet):
+    serializer_class = CategorySerializer
+    permission_classes = [IsOwner]
+
+    def get_queryset(self):
+        return Category.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class TransactionViewSet(ModelViewSet):
+    serializer_class = TransactionSerializer
+    permission_classes = [IsOwner]
+    filterset_class = TransactionFilter
+    ordering_fields = ['amount', 'created_at']
+
+    def get_queryset(self):
+        return Transaction.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
